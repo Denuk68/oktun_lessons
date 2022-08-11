1ee588677b6d24c5555775ae5d183b1d5d75a8d5
Denuk68 <denus.denisovuch@gmail.com>
2022-08-11 16:36:32 +0300

    lesson_4

--- js/lesson_4/index.js
+++ js/lesson_4/index.js
@@ -277,56 +277,111 @@
 // Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
 // ==============================================
 
-let popelushki = [];
-function Popelushka(name, age, footSize) {
-    this.name = name;
-    this.age = age;
-    this.footSize = footSize;
+// let popelushki = [];
+// function Popelushka(name, age, footSize) {
+//     this.name = name;
+//     this.age = age;
+//     this.footSize = footSize;
 
-    popelushki.push(this);
-};
+//     popelushki.push(this);
+// };
 
-let prince = [];
-function Prince(name, age, footSize) {
-    this.name = name;
-    this.age = age;
-    this.footSize = footSize;
+// let prince = [];
+// function Prince(name, age, footSize) {
+//     this.name = name;
+//     this.age = age;
+//     this.footSize = footSize;
 
-    prince.push(this);
-};
+//     prince.push(this);
+// };
 
-let dataPopelushki = [
-    new Popelushka('Popelushka1', 21, 10),
-    new Popelushka('Popelushka2', 22, 3),
-    new Popelushka('Popelushka3', 23, 2),
-    new Popelushka('Popelushka4', 24, 1),
-    new Popelushka('Popelushka5', 25, 4),
-    new Popelushka('Popelushka6', 26, 5),
-    new Popelushka('Popelushka7', 27, 6),
-    new Popelushka('Popelushka8', 28, 7),
-    new Popelushka('Popelushka9', 29, 8),
-    new Popelushka('Popelushka10', 30, 9)
-];
-let dataPrince = [
-    new Prince('Prince1', 25, 1),
-    new Prince('Prince2', 24, 2),
-    new Prince('Prince3', 23, 3),
-    new Prince('Prince4', 24, 4),
-    new Prince('Prince5', 25, 5),
-    new Prince('Prince6', 26, 6),
-    new Prince('Prince7', 27, 7),
-    new Prince('Prince8', 28, 8),
-    new Prince('Prince9', 29, 9),
-    new Prince('Prince10', 30, 10)
-];
+// let dataPopelushki = [
+//     new Popelushka('Popelushka1', 21, 10),
+//     new Popelushka('Popelushka2', 22, 3),
+//     new Popelushka('Popelushka3', 23, 2),
+//     new Popelushka('Popelushka4', 24, 1),
+//     new Popelushka('Popelushka5', 25, 4),
+//     new Popelushka('Popelushka6', 26, 5),
+//     new Popelushka('Popelushka7', 27, 6),
+//     new Popelushka('Popelushka8', 28, 7),
+//     new Popelushka('Popelushka9', 29, 8),
+//     new Popelushka('Popelushka10', 30, 9)
+// ];
+// let dataPrince = [
+//     new Prince('Prince1', 25, 1),
+//     new Prince('Prince2', 24, 2),
+//     new Prince('Prince3', 23, 3),
+//     new Prince('Prince4', 24, 4),
+//     new Prince('Prince5', 25, 5),
+//     new Prince('Prince6', 26, 6),
+//     new Prince('Prince7', 27, 7),
+//     new Prince('Prince8', 28, 8),
+//     new Prince('Prince9', 29, 9),
+//     new Prince('Prince10', 30, 10)
+// ];
 
-console.log(popelushki);
-console.log(prince);
+// console.log(popelushki);
+// console.log(prince);
 
-for (let girl of popelushki) {
-    for (let boy of prince) {
-        if (girl.footSize === boy.footSize) {
-            console.log(`Попелюшка ${girl.name} повинна бути з ${boy.name}`)
-        }
-    }
-};
+// for (let girl of popelushki) {
+//     for (let boy of prince) {
+//         if (girl.footSize === boy.footSize) {
+//             console.log(`Попелюшка ${girl.name} повинна бути з ${boy.name}`)
+//         }
+//     }
+// };
+
+
+// ==============================================
+// - Створити клас або функцію конструктор, за допомоги якої можна створювати об'єкти наступного вигляду.
+// Конструктор повинен приймати значення для кожної властивості, в т.ч і для властивостей внутрішніх об'єктів
+
+// {
+//     id: 1,
+//     name: 'Leanne Graham',
+//     username: 'Bret',
+//     email: 'Sincere@april.biz',
+//     address: {
+//         street: 'Kulas Light',
+//         suite: 'Apt. 556',
+//         city: 'Gwenborough',
+//         zipcode: '92998-3874',
+//         geo: {
+//             lat: '-37.3159',
+//             lng: '81.1496'
+//         }
+//     },
+//     phone: '1-770-736-8031 x56442',
+//     website: 'hildegard.org',
+//     company: {
+//         name: 'Romaguera-Crona',
+//         catchPhrase: 'Multi-layered client-server neural-net',
+//         bs: 'harness real-time e-markets'
+//     }
+// }
+// ==============================================
+
+// function createUser(id, name, username, email, addressStreet, addressSuite, addressCity, addressZipcode, addressGeoLat, addressGeoLng, phone, website, companyName, companyCatchPhrase, companyBs) {
+//     this.id = id;
+//     this.name = name;
+//     this.username = username;
+//     this.email = email;
+//     this.address = {};
+//     this.address.street = addressStreet;
+//     this.address.suite = addressSuite;
+//     this.address.city = addressCity;
+//     this.address.zipcode = addressZipcode;
+//     this.address.geo = {};
+//     this.address.geo.lat = addressGeoLat;
+//     this.address.geo.lng = addressGeoLng;
+//     this.phone = phone;
+//     this.website = website;
+//     this.company={};
+//     this.company.name = companyName;
+//     this.company.catchPhrase = companyCatchPhrase;
+//     this.company.bs = companyBs;
+// };
+
+// let user = new createUser(1, "Leanne Graham", "Bret", "Sincere@april.biz", "Kulas Light",
+//     "Apt. 556", "Gwenborough", "92998-3874", -37.3159, 81.1496, "1-770-736-8031 x56442", "hildegard.org", "Romaguera-Crona", "Multi-layered client-server neural-net", "harness real-time e-markets");
+// console.log(user);
