b682106924b0b6f887795f4b52536d46b8efaf2e
Denuk68 <denus.denisovuch@gmail.com>
2022-08-12 22:58:10 +0300

    lesson 5 js

--- js/lesson_5/template2.js
+++ js/lesson_5/template2.js
@@ -17,7 +17,6 @@
 // ============================================================
 
 let h1 = document.getElementById("main_header");
-console.log(h1);
 
 h1.innerText = "DOM JS";
 h1.classList.add("h1_color");
@@ -26,7 +25,7 @@ let ul = document.getElementsByTagName("ul");
 ul[0].classList.add("ul_width");
 
 let linkList = document.getElementsByClassName("linkList");
-for (let i of linkList){
+for (let i of linkList) {
     i.classList.add('link_list');
 };
 
@@ -36,21 +35,44 @@ listElement2[0].innerText = textListElement2;
 
 
 let li = document.getElementsByTagName("li");
-for(let i of li){
+for (let i of li) {
     i.classList.add("li_grey");
 }
 
 let a = document.getElementsByTagName("a");
-for(let i of a){
-    console.log(i)
+for (let i of a) {
     i.classList.add("anchor");
-    if(i.innerText === "link3"){
+    if (i.innerText === "link3") {
         i.classList.add("a_fontSize")
     }
 }
-for(let i of a){
+for (let i of a) {
     let info = i.innerText;
     let name = "element_" + info;
     i.classList.add(name)
 }
 
+let subHeader = document.getElementsByClassName("sub-header");
+let backgoundColor = prompt("Enter background-color for sub-header", "blue");
+for (let i of subHeader) {
+    i.style.backgroundColor = backgoundColor;
+}
+
+let color = prompt("Enter sub-header color ", "red");
+for (let i of subHeader) {
+    if (i.innerHTML = "content 2 segment") {
+        i.style.color = color;
+    }
+}
+
+let content_1 = document.getElementsByClassName("content_1");
+let textContent_1 = prompt("Enter text content", "some text");
+content_1[0].innerText = textContent_1;
+
+let p = document.getElementsByTagName("p");
+for (let i of p) {
+    i.style.padding = "20px"
+}
+
+let text2 = document.getElementsByClassName("text2");
+text2[0].innerHTML = "Im Denuk68"
